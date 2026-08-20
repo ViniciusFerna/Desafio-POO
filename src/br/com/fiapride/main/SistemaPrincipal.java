d5cef63970aed2ae5fb12bdfc743ce82ee19bd6c
Vinícius Fernandes <vinicius020206@gmail.com>
2026-08-20 16:32:50 -0300

    Testes atualizados

--- src/br/com/fiapride/main/SistemaPrincipal.java
+++ src/br/com/fiapride/main/SistemaPrincipal.java
@@ -10,13 +10,15 @@ public class SistemaPrincipal {
 		Passageiro passageiro1 = new Passageiro();
 		passageiro1.nome = "Ana Silva";
 		passageiro1.saldo = 50.00;
-		passageiro1.tamanhoCalcado = 2;
+		passageiro1.tamanhoCalcado = 1;
 		
 		passageiro1.validarSaldoViagem(passageiro1.saldo, 200);
 		passageiro1.validarIdade(16);
 		
 		passageiro1.mudarSaldo(250);
 		passageiro1.validarSaldoViagem(passageiro1.saldo, 200);
+		
+		System.out.println("----------------------------------");
 		
 		Tenis tenis1 = new Tenis();
 		tenis1.modelo = "Yeezy 350";
@@ -29,6 +31,30 @@ public class SistemaPrincipal {
 		tenis1.validarTamanho(passageiro1.tamanhoCalcado);
 		
 		System.out.println(tenis1.modelo);
+		
+		System.out.println("----------------------------------");
+		
+		Tenis tenis2 = new Tenis();
+		tenis2.modelo = "Yeezy 450";
+		tenis2.cor = "bege";
+		tenis2.tamanho = 1;
+		
+		Passageiro passageiro2 = new Passageiro();
+		passageiro2.nome = "Pablo";
+		passageiro2.saldo = 0.00;
+		passageiro2.tamanhoCalcado = 1;
+		
+		if (tenis2.validarTamanho(passageiro2.tamanhoCalcado) == 6) {
+			System.out.println("Pode comprar");
+		}
+		
+		System.out.println("----------------------------------");
+		
+		tenis2.validarTamanho(-2);
+		
+		
+		
+		
 	}
 
 }
--- src/br/com/fiapride/model/Tenis.java
+++ src/br/com/fiapride/model/Tenis.java
@@ -19,20 +19,23 @@ public class Tenis {
 		}
 	}
 	
+	
 	public int validarTamanho(int tamanhoCliente) { // Simplificado: 1 - P / 2 - M / 3 - G / 6 - True / 7 - False / 0 - Fail
-		 if (tamanhoCliente == this.tamanho) {
-			 System.out.println("Tamanho perfeito");
-			 return 6;
-		 } else if ( tamanhoCliente > this.tamanho) {
-			 System.out.println("Esse tenis e muito pequeno!");
-			 return 7;
-		 } else if (tamanhoCliente < this.tamanho) {
-			 System.out.println("Esse tenis e muito grande!");
-			 return 7;
-		 } else {
+		 if (tamanhoCliente <= 0) {
 			 System.out.println("Apresente um valor valido");
 			 return 0;
+		 } else if (tamanhoCliente == this.tamanho) {
+			 System.out.println("Tamanho perfeito");
+			 return 6;
+		 } else if (tamanhoCliente > this.tamanho) {
+			 System.out.println("Esse tenis e muito pequeno!");
+			 return 7;
+		 } else {
+			 System.out.println("Esse tenis e muito grande");
+			 return 7;
 		 }
+		 
+		 
 		 
 	}
 	
